8811342a99c00a053c2dbe1162162e13c7c738a8
baonguyenyam@gmail.com <baonguyenyam@gmail.com>
2016-06-12 15:27:15 +0700

    UP

--- resources/assets/scripts/loading.js
+++ resources/assets/scripts/loading.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     setTimeout(function() {
         var loader = document.getElementsByClassName("loading")[0];
-        loader.className = "loading fadeout";
+        loader.className = "loading animated fadeOut";
         setTimeout(function() {
             loader.style.display = "none"
         }, 1000)
