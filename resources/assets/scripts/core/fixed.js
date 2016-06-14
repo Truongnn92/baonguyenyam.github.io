0bb7c4cef5a46bfb732d7019fea26b34a2fcf47a
Nguyen Pham <baonguyenyam@gmail.com>
2016-06-14 10:47:10 +0700

    UP

--- resources/assets/scripts/core/fixed.js
+++ resources/assets/scripts/core/fixed.js
@@ -4,16 +4,16 @@
 
 
 $(document).ready(function() {
-    $(".info-page header, .gallery-page header, .coding-page header").addClass('active');
+    $(".info-page header, .gallery-page header, .coding-page header, .poetry-page header").addClass('active');
     $('.scrollbar-dynamic, .scrollbar-inner').perfectScrollbar();
 });
 
 
 $(document).on('ps-y-reach-start', function() {
-    $(".info-page header, .gallery-page header, .coding-page header").addClass('active');
+    $(".info-page header, .gallery-page header, .coding-page header, .poetry-page header").addClass('active');
 });
 $(document).on('ps-scroll-down', function() {
-    $(".info-page header, .gallery-page header, .coding-page header").removeClass('active');
+    $(".info-page header, .gallery-page header, .coding-page header, .poetry-page header").removeClass('active');
 });
 
 $(function() {
